fa42524e3342acf4eef75884d8e6dbe02a864805
Nurbolot Boobekov <nurbolot.boobekov.pls@gmail.com>
2022-03-05 13:10:19 +0300

    create header, add sccs styles and burger menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,18 @@
 import * as flsFunctions from "./modules/functions.js";
-
 flsFunctions.isWebp();
+
+const burger = document.querySelector(".header__burger");
+const menu = document.querySelector(".header__menu");
+const body = document.querySelector("body");
+const items = document.querySelectorAll(".header__item");
+
+burger.addEventListener("click", () => {
+  [burger, menu].map((e) => e.classList.toggle("active"));
+  body.classList.toggle("lock");
+});
+
+for (const el of items) {
+  el.addEventListener("click", () => {
+    [burger, menu].map((element) => element.classList.remove("active"));
+  });
+}
